25e3bcdf9622deb78b20390191db7225624bcf9f
C1DeChand <C1DeChand@gmail.com>
2020-06-16 19:24:05 -0700

    updated db

--- db/schema.sql
+++ db/schema.sql
@@ -1,9 +1,6 @@
-#drop database if exists Message;
-#create database Message;
+drop database if exists Message;
+create database Message;
 
 use Message;
 
-insert into messages (username, message, sentiment, likes) values ("test 1", "dagon rahl", 0, 23);
-insert into messages (username, message, sentiment, likes, dislikes) values ("test 2", "sasquach", 1, 2, 50);
-
-select * from messages
+select * from messages;
