3d96517aa4d7d078a921414ac11cdd94b3ee636e
Andorask <andrewpadgett94@gmail.com>
2020-06-17 13:53:03 -0700

    Refactored project

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -46,23 +46,20 @@ module.exports = function(app) {
 };
 
 async function getSentiment(text) {
-  // Imports the Google Cloud client library
   const language = require("@google-cloud/language");
-  ​
-  // Instantiates a client
+
   const client = new language.LanguageServiceClient();
-  ​
+
   const document = {
-    content: text,
+    content: text, 
     type: "PLAIN_TEXT"
   };
-  ​
-  // Detects the sentiment of the text
-  const [result] = await client.analyzeSentiment({ document: document });
+
+  const [result] = await client.analyzeSentiment({ document: document});
   const sentiment = result.documentSentiment;
-  ​
+
   console.log(`Text: ${text}`);
   console.log(`Sentiment score: ${sentiment.score}`);
   console.log(`Sentiment magnitude: ${sentiment.magnitude}`);
   return sentiment;
-  }
+}
--- server.js
+++ server.js
@@ -5,17 +5,18 @@ const db = require("./models");
 
 // Creating express app and configuring middleware needed for authentication
 const app = express();
-
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static(__dirname + '/public'));
 var exphbs = require("express-handlebars");
 app.engine("handlebars", exphbs({ defaultLayout: "main"}));
 app.set("view engine", "handlebars");
-app.use(express.static(__dirname + '/public'));
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
+
 //app.use(express.static("public"));
 
 require("./routes/html-routes.js")(app)
 require("./routes/api-routes.js")(app);
+
 
 // Syncing our database and logging a message to the user upon success
 db.sequelize.sync().then(() => {
