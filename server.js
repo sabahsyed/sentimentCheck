fce8bd51072a48133e4b996694876f99a5bde570
Sabah Syed <sabisyed@gmail.com>
2020-06-18 16:17:01 -0700

    Dylan's changes added

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const exphbs = require("express-handlebars");
 
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 3000;
 const db = require("./models");
 
 const app = express();
